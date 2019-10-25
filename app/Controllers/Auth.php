f459c3670226c54c8a5a25b3fc1967dae5404b46
jemai-eyoub <jemai.eyoub@gmail.com>
2019-10-25 17:51:24 +0100

    dd

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -1,84 +1,84 @@
-<?php
-
-
-namespace App\Controllers;
-
-use CodeIgniter\Controller;
-use Config\Services;
-
-class Auth extends Controller
-{
-
-	private $validation;
-
-	public function __construct(Services $services)
-	{
-		$this->validation = $services::validation();
-	}
-	public function login(): void
-	{
-
-		$this->validation->setRules([
-			'userName' => [
-				'label' => 'first name',
-				'rules' => 'required|alpha|min_length[8]',
-			],
-		],
-			[
-			'lastName' => [
-				'label' => 'Last name',
-				'rules' => 'required|alpha|min_length[8]',
-			],
-			]);
-
-		if (! $this->validation->run())
-		{
-			redirect('logout');
-		}
-		else
-		{
-			echo 'success';
-		}
-	}
-
-	public function attemptLogin(): void
-	{
-	}
-
-	public function logout()
-	{
-		helper('url');
-		$data = [
-			'pseudo' => 'Arthur',
-			'email'  => 'dd@dd.dd',
-			'online' => true,
-		];
-
-		return view('auth/logout', $data);
-	}
-
-	public function register(): void
-	{
-	}
-
-	public function attemptRegister(): void
-	{
-	}
-
-	public function forgotPassword(): void
-	{
-	}
-
-	public function attemptForgot(): void
-	{
-	}
-
-	public function resetPassword(): void
-	{
-	}
-
-	public function attemptReset(): void
-	{
-	}
-
-}
+//<?php
+//
+//
+//namespace App\Controllers;
+//
+//use CodeIgniter\Controller;
+//use Config\Services;
+//
+//class Auth extends Controller
+//{
+//
+//	private $validation;
+//
+//	public function __construct(Services $services)
+//	{
+//		$this->validation = $services::validation();
+//	}
+//	public function login(): void
+//	{
+//
+//		$this->validation->setRules([
+//			'userName' => [
+//				'label' => 'first name',
+//				'rules' => 'required|alpha|min_length[8]',
+//			],
+//		],
+//			[
+//			'lastName' => [
+//				'label' => 'Last name',
+//				'rules' => 'required|alpha|min_length[8]',
+//			],
+//			]);
+//
+//		if (! $this->validation->run())
+//		{
+//			redirect('logout');
+//		}
+//		else
+//		{
+//			echo 'success';
+//		}
+//	}
+//
+//	public function attemptLogin(): void
+//	{
+//	}
+//
+//	public function logout()
+//	{
+//		helper('url');
+//		$data = [
+//			'pseudo' => 'Arthur',
+//			'email'  => 'dd@dd.dd',
+//			'online' => true,
+//		];
+//
+//		return view('auth/logout', $data);
+//	}
+//
+//	public function register(): void
+//	{
+//	}
+//
+//	public function attemptRegister(): void
+//	{
+//	}
+//
+//	public function forgotPassword(): void
+//	{
+//	}
+//
+//	public function attemptForgot(): void
+//	{
+//	}
+//
+//	public function resetPassword(): void
+//	{
+//	}
+//
+//	public function attemptReset(): void
+//	{
+//	}
+//
+//}
--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -1,79 +1,79 @@
-<?php
-
-namespace App\Controllers;
-
-use CodeIgniter\Controller;
-use Config\Database;
-
-class Test extends Controller
-{
-
-	protected $db;
-
-	public function __construct()
-	{
-		$this->db = Database::connect();
-		//       To use outside Model View Controller classes [THE SUPER OBJECT CI]
-		//            $CI =& get_instance();
-		//            $CI->load->helper('url');
-		//            $CI->log_message('', 'message');
-		//      $cls = new \Cls();
-		//      $cls->cls1();
-		//      helper('assets');
-		//      cssUrl();
-		//      getArrayHelepr();
-
-}
-
-	public function query() : void
-	{
-		cssUrl();
-		// Query generator
-		$query    = $this->db->query('select * from students where id in ? and name like ?', [[1, 2], 's%']);
-		$students = $query->getResult();
-
-		// Array of objects
-		foreach ($students as $student)
-		{
-			echo $student->name;
-			echo $student->subject;
-			echo $student->age;
-			echo $student->date;
-			echo $student->update . '<br>';
-		}
-
-		// Array of associative arrays
-		$students = $query->getResultArray();
-		foreach ($students as $student)
-		{
-			echo $student['name'];
-			echo $student['subject'];
-			echo $student['age'];
-			echo $student['date'];
-			echo $student['update'] . '<br>';
-		}
-		$this->db->close();
-	}
-
-	public function queryBuilder() : void
-	{
-		$builder = $this->db->table('students');
-		$builder->select(['id', 'name', 'subject'])
-			->where(['name' => 'adel', 'id' => 1])  // AND
-			->orWhere('id', 12)
-			->orWhere('name', 'dd dd');
-		$res = $builder->get()->getResult();
-		d($this->db->getLastQuery());
-		d($res);
-
-		$builder1 = $this->db->table('students');
-		//      $res1 = $builder->select('age');
-		$res1 = $builder1->selectAvg('age');
-		//      $builder->countAll();
-		//      $res = $builder->get()->getResult();
-		 // Get ID of last inserted row
-		//      $this->db->insertID();
-		d($this->db->getLastQuery());
-		d($res1);
-	}
-}
+//<?php
+//
+//namespace App\Controllers;
+//
+//use CodeIgniter\Controller;
+//use Config\Database;
+//
+//class Test extends Controller
+//{
+//
+//	protected $db;
+//
+//	public function __construct()
+//	{
+//		$this->db = Database::connect();
+//		//       To use outside Model View Controller classes [THE SUPER OBJECT CI]
+//		//            $CI =& get_instance();
+//		//            $CI->load->helper('url');
+//		//            $CI->log_message('', 'message');
+//		//      $cls = new \Cls();
+//		//      $cls->cls1();
+//		//      helper('assets');
+//		//      cssUrl();
+//		//      getArrayHelepr();
+//
+//}
+//
+//	public function query() : void
+//	{
+//		cssUrl();
+//		// Query generator
+//		$query    = $this->db->query('select * from students where id in ? and name like ?', [[1, 2], 's%']);
+//		$students = $query->getResult();
+//
+//		// Array of objects
+//		foreach ($students as $student)
+//		{
+//			echo $student->name;
+//			echo $student->subject;
+//			echo $student->age;
+//			echo $student->date;
+//			echo $student->update . '<br>';
+//		}
+//
+//		// Array of associative arrays
+//		$students = $query->getResultArray();
+//		foreach ($students as $student)
+//		{
+//			echo $student['name'];
+//			echo $student['subject'];
+//			echo $student['age'];
+//			echo $student['date'];
+//			echo $student['update'] . '<br>';
+//		}
+//		$this->db->close();
+//	}
+//
+//	public function queryBuilder() : void
+//	{
+//		$builder = $this->db->table('students');
+//		$builder->select(['id', 'name', 'subject'])
+//			->where(['name' => 'adel', 'id' => 1])  // AND
+//			->orWhere('id', 12)
+//			->orWhere('name', 'dd dd');
+//		$res = $builder->get()->getResult();
+//		d($this->db->getLastQuery());
+//		d($res);
+//
+//		$builder1 = $this->db->table('students');
+//		//      $res1 = $builder->select('age');
+//		$res1 = $builder1->selectAvg('age');
+//		//      $builder->countAll();
+//		//      $res = $builder->get()->getResult();
+//		 // Get ID of last inserted row
+//		//      $this->db->insertID();
+//		d($this->db->getLastQuery());
+//		d($res1);
+//	}
+//}
